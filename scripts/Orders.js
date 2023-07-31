737f63b5e4fd18dabe734626b272b8a52a455cd4
Michelle Primiani <primianimichelle@gmail.com>
2023-07-31 16:47:43 -0400

    initial commit

---
+++ scripts/Orders.js
@@ -0,0 +1,58 @@
+import { getProducts, getEmployees, getOrders } from "./database.js";
+
+// Get copy of state for use in this module
+const products = getProducts();
+const employees = getEmployees();
+const orders = getOrders();
+
+// Function whose responsibility is to find the product for an order
+const findProduct = (order, allProducts) => {
+  let orderProduct = null;
+
+  for (const product of products) {
+    if (product.id === order.productId) {
+      orderProduct = product;
+    }
+  }
+
+  return orderProduct;
+};
+
+// Function whose responsibility is to find the employee for an order
+const findEmployee = (order, allEmployees) => {
+  let orderEmployee = null;
+
+  for (const employee in allEmployees) {
+    if (employee.id === order.employeeId) {
+      orderEmployee = employee;
+    }
+  }
+
+  return orderEmployee;
+};
+
+export const Orders = () => {
+  let html = "";
+  html = "<ul>";
+
+  for (const order of orders) {
+    const employee = findEmployee(order, employees);
+    const product = findProduct(order);
+    if (employee === null) {
+      html += `<li>${product.name} was sold on ${new Date(
+        order.timestamp
+      ).toLocaleDateString()}</li>`;
+    } else {
+      html += `<li>${product.name} was sold by ${employee.name} on ${new Date(
+        order.timestamp
+      ).toLocaleDateString()}</li>`;
+    }
+  }
+
+  html += "</ul>";
+
+  return html;
+};
+
+// if else statement
+// if employee name is not === to employee id
---
+++ scripts/Products.js
@@ -0,0 +1,14 @@
+import { getProducts } from "./database.js";
+
+const products = getProducts();
+
+export const Products = () => {
+  let html = `<ul>`;
+
+  for (const product of products) {
+    html += `<li>${product.name}</li>`;
+  }
+
+  html += "</ul>";
+  return html;
+};
